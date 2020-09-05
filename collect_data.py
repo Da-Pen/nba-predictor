21bcd00469b45d5a3dbbb88913e46d8aa8dbe081
Daniel Peng <danielpeng@Daniels-MacBook-Pro.local>
2020-09-05 18:38:31 -0400

    some cleanup and update requirements.txt

--- collect_data.py
+++ collect_data.py
@@ -1,18 +1,13 @@
 import os
 
-from nba_api.stats.endpoints import teamgamelogs, teamdashboardbyteamperformance, leaguegamelog
-from nba_api.stats.library.parameters import PerModeDetailed
+from nba_api.stats.endpoints import teamgamelogs, leaguegamelog
 from nba_api.stats.static import teams
 from time import sleep
 import numpy as np
 
 from constants import data_directory, output_file, input_file, relevant_team_perf_metrics, stats_to_be_averaged
 from team_stats import GameOverview
-from utils.dates import get_season_start_and_end_dates, start_year_to_season, get_season_for_date
-
-# # directories to write the data files to
-# lost_directory = "lost"
-# won_directory = "won"
+from utils.dates import get_season_start_and_end_dates, start_year_to_season
 
 
 # converts a list of headers and data into a map of header:data
@@ -74,32 +69,6 @@ def get_league_gamelogs(season_start_year):
     return new_gamelogs
 
 
-'''
-# writes the raw (not yet preprocessed) input data for a given team and season to files
-def write_inputs_for_season(team_id, season_start_year):
-    # get list of all the games and whether they won or lost
-    gamelogs = reformat_response(get_team_gamelogs_for_season(team_id, season_start_year))
-    for game in gamelogs:
-        pass
-        # get date of the game
-        date = game['GAME_DATE']
-        # get this team's stats at that point in time
-        team_stats = get_team_perf(team_id, date_to=date)
-        # get opponent's stats at that point in time
-        # opp
-        opp_stats = get_team_perf()
-
-
-
-def get_team_perf(team_id, date_to):
-    t = teamdashboardbyteamperformance.TeamDashboardByTeamPerformance(
-        team_id=team_id,
-        date_to_nullable=date_to,
-        season=get_season_for_date(date_to),
-        per_mode_detailed=PerModeDetailed.per_game)
-    return reformat_response(t.overall_team_dashboard.get_dict())[0]
-'''
-
 # given a team's data (dict), returns a list that can be saved
 def encode_data(team1_data, team2_data):
     # extract relevant data from raw data dict
@@ -107,27 +76,6 @@ def encode_data(team1_data, team2_data):
     return [team1_data[metric] for metric in relevant_team_perf_metrics] + \
         [team2_data[metric] for metric in relevant_team_perf_metrics]
 
-'''
-# writes team performance data to file and returns the file names
-def write_team_perf_to_files(game_id, winning_team_stats, losing_team_stats):
-    # create the directories if they don't exist
-    if not os.path.exists(f"{data_directory}/{won_directory}"):
-        os.makedirs(f"{data_directory}/{won_directory}")
-    if not os.path.exists(f"{data_directory}/{lost_directory}"):
-        os.makedirs(f"{data_directory}/{lost_directory}")
-
-    # create winning teams file
-    file_name_winning = f"{data_directory}/{won_directory}/{game_id}.npy"
-    winning_arr = np.array(encode_data(winning_team_stats, losing_team_stats))
-    np.save(file_name_winning, winning_arr)
-
-    # create losing teams file
-    file_name_losing = f"{data_directory}/{lost_directory}/{game_id}.npy"
-    losing_arr = np.array(encode_data(losing_team_stats, winning_team_stats))
-    np.save(file_name_losing, losing_arr)
-
-    return file_name_winning, file_name_losing
-'''
 
 # given a list of games for a team in chronological order, returns a dict which maps a game id to the accumulated stats
 # of the team up to but not including the date of the game.
@@ -181,8 +129,6 @@ def main():
 
             winning_input.append((winning_team_data, losing_team_data))
             losing_input.append((losing_team_data, winning_team_data))
-            # winning_team_file, losing_team_file = write_team_perf_to_files(game.game_id, winning_team_stats, losing_team_stats)
-            # print('wrote', game, 'to files', winning_team_file, "and", losing_team_file)
 
         else:
             seen_teams.add(game.winning_team_id)
@@ -196,40 +142,6 @@ def main():
     np.save(input_file, np.array([encode_data(team1, team2) for team1, team2 in winning_input + losing_input]))
     # write output to file
     np.save(output_file, np.array([1]*len(winning_input) + [0]*len(losing_input)))
-
-
-    # team_ids = get_team_abbrev_to_id_map()
-    # print(get_team_perf(team_ids['ATL'], "03/10/2020"))
-    # print(team_ids)
-    # team_gamelogs = get_team_gamelogs_for_season(
-    #     team_ids['TOR'],
-    #     2019)
-    # print(team_gamelogs)
-
-    '''
-    print(reformat_response(get_team_perf(team_ids['TOR'], '2020-05-14T00:00:00')))
-    
-    
-    
-    games = get_league_gamelogs(2019)
-    # keep list of all seen teams so that we don't use stats for a team that has not played any games yet as input
-    seen_teams = set()
-    for game in games:
-        if game.winning_team_id in seen_teams and game.losing_team_id in seen_teams:
-            y = yesterday(game.date)
-            sleep(0.5)
-            # get winning team stats
-            winning_team_stats = get_team_perf(game.winning_team_id, y)
-            winning_team_file = write_team_perf_to_file(True, game.game_id, winning_team_stats)
-            sleep(0.5)
-            # get losing team stats
-            losing_team_stats = get_team_perf(game.losing_team_id, y)
-            losing_team_file = write_team_perf_to_file(False, game.game_id, losing_team_stats)
-            print('wrote', game, 'to files', winning_team_file, "and", losing_team_file)
-        else:
-            seen_teams.add(game.winning_team_id)
-            seen_teams.add(game.losing_team_id)
-    '''
 
 
 if __name__ == '__main__':
--- machine_learning/main.py
+++ machine_learning/main.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 from sklearn.model_selection import StratifiedKFold, cross_val_score
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
-from tensorflow.keras.layers.experimental.preprocessing import Normalization
 import numpy as np
 from tensorflow.python.keras.wrappers.scikit_learn import KerasClassifier
 
@@ -31,16 +30,6 @@ def main():
     input_arr = input_arr[p]
     output_arr = output_arr[p]
 
-    # normalizer = Normalization(axis=-1)
-    # normalizer.adapt(input_arr)
-    # input_arr = normalizer(input_arr)
-
-    # evaluate model with standardized dataset
-    # estimator = KerasClassifier(build_fn=baseline_model, epochs=10, batch_size=batch_size, verbose=0)
-    # kfold = StratifiedKFold(n_splits=10, shuffle=True)
-    # results = cross_val_score(estimator, input_arr, output_arr, cv=kfold)
-    # print("Baseline: %.2f%% (%.2f%%)" % (results.mean() * 100, results.std() * 100))
-
     # evaluate baseline model with standardized dataset
     estimators = [
         ('standardize', StandardScaler()),
@@ -50,23 +39,6 @@ def main():
     kfold = StratifiedKFold(n_splits=10, shuffle=True)
     results = cross_val_score(pipeline, input_arr, output_arr, cv=kfold)
     print("Standardized: %.2f%% (%.2f%%)" % (results.mean() * 100, results.std() * 100))
-
-    # for i in range(1500, 1510):
-    #     print(input_arr[i], output_arr[i])
-
-    # model.compile(optimizer=tf.keras.optimizers.Adam(), loss='binary_crossentropy', metrics=['accuracy'])
-    # model.fit(x=input_arr, y=output_arr, validation_split=0.15, batch_size = batch_size, epochs=30, verbose=2, shuffle=False)
-    #
-    # predictions = model.predict(x=input_arr, batch_size=batch_size)
-    # print(predictions)
-    # rounded_predictions = np.argmin(predictions, axis=-1)
-    # correct_predictions = 0
-    # for i in range(len(predictions)):
-    #     if rounded_predictions[i] == output_arr[i]:
-    #         correct_predictions += 1
-    # print(f'correct predictions percentage: {correct_predictions/len(predictions)}')
-
-
 
 
 if __name__ == '__main__':
